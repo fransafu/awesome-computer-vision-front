9191d8392a1470b9312b11e98a1eeeea1411af11
Francisco Sanchez <fransafu@gmail.com>
2022-09-18 14:47:27 -0300

    feat: Update Home description

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,19 @@
+import { Col, Row, Typography } from 'antd';
+
+const { Title, Paragraph } = Typography;
+
 const Home = () => (
   <>
-    <h1>Home</h1>
+      <Row justify='center'>
+        <Col span={20}>
+          <Typography>
+            <Title>Home</Title>
+            <Paragraph>
+              Awesome Computer vision es un proyecto que recolecta contenido relacionado a visión por computadora con el objetivo de facilitar las conexiones entre los proyectos ya propuestas y los que vendrán
+            </Paragraph>
+          </Typography>
+        </Col>
+      </Row>
   </>
 );
 
