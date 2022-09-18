46433634a54bc7a30a995d2e6c4052c4a1d0e0a4
Francisco Sanchez <fransafu@gmail.com>
2022-09-18 14:44:18 -0300

    feat: Update Footer content

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,9 @@
+import { Layout } from 'antd';
+
+const { Footer } = Layout;
+
+const FooterLayout = () => (
+  <Footer style={{ textAlign: 'center' }}>Awesome computer vision ©2022 Created by Fransafu</Footer>
+);
+
+export default FooterLayout;
